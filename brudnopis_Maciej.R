b400c4a6bd180d96d7ac6f941f7a7bcdb659316c
BorkowskiMaciej <maciej.borkowski@op.pl>
2022-05-13 11:15:16 +0200

    Update brudnopis_Maciej.R

--- brudnopis_Maciej.R
+++ brudnopis_Maciej.R
@@ -1,6 +1,8 @@
 library(data.table)
 library(stringi)
 library(ggplot2)
+library(cowplot)
+
 
 # x07 <- as.data.table(`201907`)[,
 #   .(Gender = fcase( 
@@ -23,19 +25,36 @@ library(ggplot2)
 #     default = 0L))
 #   ), Gender]
 
-x <- as.data.table(rbind(`201901`, `201902`, `201903`, `201904`, `201905`, `201906`, `201907`,
-                         `201908`, `201909`, `201910`, `201911`,`201912`))[, .(
+data <- rbind(`201901`, `201902`, `201903`, `201904`, `201905`, `201906`, `201907`,
+              `201908`, `201909`, `201910`, `201911`,`201912`)
+x <- as.data.table(data)[, .(
                               TypeOfTrip = fcase(
-                                 tripduration <= 600, "Short",
-                                 tripduration > 600 & tripduration <= 1800, "Medium",
-                                 default = "Long"),
+                                 tripduration <= 480, "1-UltraShort",
+                                 tripduration > 480 & tripduration <= 900, "2-Short",
+                                 tripduration > 900 & tripduration <= 1800, "3-Medium",
+                                 tripduration > 1800 & tripduration <= 3600, "4-Long",
+                                 default = "5-UltraLong"),
                               Season = fcase(
                                 stri_sub(starttime, 6, 7) == "01" | stri_sub(starttime, 6, 7) == "02" | stri_sub(starttime, 6, 7) == "12", "Winter",
                                 stri_sub(starttime, 6, 7) == "03" | stri_sub(starttime, 6, 7) == "04" | stri_sub(starttime, 6, 7) == "05", "Spring",
                                 stri_sub(starttime, 6, 7) == "06" | stri_sub(starttime, 6, 7) == "07" | stri_sub(starttime, 6, 7) == "08", "Summer",
                                 default = "Autumn"
-                                ))][, .(Count = .N), .(Season, TypeOfTrip)][,.(Season, TypeOfTrip, Count = Count/3)]
+                                ))][, .(Count = .N), .(Season, TypeOfTrip)][,.(Season, TypeOfTrip, AverageCount = Count/3)]
 x
 
-ggplot(data = x, aes(Season, Count)) + geom_bar(stat = "identity", position = "dodge", aes(fill=TypeOfTrip)) +
-  labs(x = "Season", y="Count") + scale_x_discrete(limits = c("Spring", "Summer", "Autumn", "Winter"))
+allgenders <- ggplot(data = x, aes(Season, AverageCount)) + geom_bar(stat = "identity", position = "dodge", aes(fill=TypeOfTrip)) +
+  labs(x = "Season", y="AverageCount") + scale_x_discrete(limits = c("Spring", "Summer", "Autumn", "Winter"))
+
+men <- ggplot(data = x, aes(Season, AverageCount)) + geom_bar(stat = "identity", position = "dodge", aes(fill=TypeOfTrip)) +
+  labs(x = "Season", y="AverageCount") + scale_x_discrete(limits = c("Spring", "Summer", "Autumn", "Winter"))
+
+women <- ggplot(data = x, aes(Season, AverageCount)) + geom_bar(stat = "identity", position = "dodge", aes(fill=TypeOfTrip)) +
+  labs(x = "Season", y="AverageCount") + scale_x_discrete(limits = c("Spring", "Summer", "Autumn", "Winter"))
+
+undefined <- ggplot(data = x, aes(Season, AverageCount)) + geom_bar(stat = "identity", position = "dodge", aes(fill=TypeOfTrip)) +
+  labs(x = "Season", y="AverageCount") + scale_x_discrete(limits = c("Spring", "Summer", "Autumn", "Winter"))
+
+plot_grid(allgenders, men, women, undefined, labels = c("All genders", "Men", "Women", "Undefined"))
+
+
+
